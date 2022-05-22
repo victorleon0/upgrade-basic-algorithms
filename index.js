10b0decbba6fcb2765109b75be228979818fe47d
victorleon0 <victor.leon.huerta@gmail.com>
2022-05-22 12:55:06 +0100

    Iteration 6

--- index.js
+++ index.js
@@ -148,3 +148,33 @@ if (((number3*5)== number1),(number1*2)== number2) {
 if ((number2/2)==number1) {
   console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
 }
+
+
+//**Iteración #6: Bucles**
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+for (let index = 0; index <= 9; index++) {
+    console.log (index);
+}
+
+//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+//cuando el resto del numero dividido entre 2 sea 0.
+
+for (let index = 0; index <= 9; index++) {
+    const divisor = (index/2)
+    console.log (divisor == 0);
+}
+
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas. 
+//Este bucle empieza en 0 y termina en 10. 
+//Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+//y cambia el mensaje en la última vuelta a 'Dormido!'.
+
+for (let ovejas = 0; ovejas <= 10; ovejas++) {
+    console.log (ovejas + "Intentando dormir");
+    if (ovejas == 10) {
+        console.log ("DORMIDO ")
+    }
+    
+}
